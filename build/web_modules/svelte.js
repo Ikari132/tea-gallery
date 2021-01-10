d211a445f01b58bbb32921783209c1915255a007
Anton Aksionau <ikari808@gmail.com>
2021-01-10 23:30:44 +0100

    add web_modules

---
+++ build/web_modules/svelte.js
@@ -0,0 +1,1 @@
+export { c as createEventDispatcher } from './common/index-54742a68.js';
---
+++ build/web_modules/svelte/store.js
@@ -0,0 +1,2 @@
+import '../common/index-54742a68.js';
+export { w as writable } from '../common/index-8a23dc1d.js';
